4c94a6e9e45dfc62e21a380436473b1a9d785f06
sonwonjae <sonwj091552@gmail.com>
2022-06-19 23:49:52 +0900

    feat: create Count Component - #4

---
+++ src/components/Count/Count.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+import { timing } from '../../utils'
+
+interface CountProps {
+  duration?: number
+  children: number
+}
+
+function Count({ duration = 2000, children }: CountProps) {
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    if (progress < 1) {
+      const frameBudget = 1000 / 60
+
+      setTimeout(() => {
+        setProgress(progress + frameBudget / duration)
+      }, frameBudget)
+    }
+  }, [duration, progress])
+
+  return <>{Math.round(children * timing.easeOut(progress))}</>
+}
+
+export default Count
