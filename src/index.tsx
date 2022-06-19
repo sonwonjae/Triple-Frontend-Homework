6634da0ead8f45048a5a98458df1368159eda5f4
sonwonjae <sonwj091552@gmail.com>
2022-06-19 23:54:17 +0900

    feat: create Homework Page Component - #6

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from 'styled-components'
 
 import Theme from './styles/theme'
 import GlobalStyle from './styles/global-styles'
+import { Homework } from './pages'
 
 ReactDOM.render(
   <ThemeProvider theme={Theme}>
     <GlobalStyle />
+    <Homework />
   </ThemeProvider>,
   document.getElementById('root'),
 )
---
+++ src/pages/index.ts
@@ -0,0 +1,1 @@
+export { default as Homework } from './Homework'
